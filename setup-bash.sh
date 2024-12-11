d1c2d6f1438cd73527aa340a059a8a2a3e8c4a14
Maximilian Krönung <mail@maximizzar.de>
2024-12-11 10:04:55 +0100

    fix typos + add nala completions

--- setup-bash.sh
+++ setup-bash.sh
@@ -1,13 +1,22 @@
 #!/usr/bin/env bash
 
+# Install bash rc
 if [ -f "$HOME/.bashrc" ]; then
         echo "Skipping .bashrc. It already exists"
 else
         ln -s "$(pwd)/.bash/rc" "$HOME"/.bashrc;
 fi
 
+# Install bash aliases
 if [ -f "$HOME/.bash_aliases" ]; then
         echo "Skipping .bash_aliases. It already exists"
 else
         ln -s "$(pwd)/.bash/aliases" "$HOME"/.bash_aliases;
+fi
+
+# Install nala bash completion
+if command -v nala &> /dev/null; then
+        if ! [[ -f "$HOME/.bash_completions/nala.sh" ]]; then
+                nala --install-completion
+        fi
 fi
