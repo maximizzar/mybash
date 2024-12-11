c06a153aebca2bdd529edc6d24f20e5d2ffe00c2
Maximilian Krönung <mail@maximizzar.de>
2024-12-11 09:37:11 +0100

    moved back to single bashrc file

--- install-bashrc.sh
+++ install-bashrc.sh
@@ -1,25 +1,5 @@
 #!/usr/bin/env bash
 
-# Don't use sudo? change the alias
-alias sudo='sudo'
-
-install_global_bashrc=yes
-if [[ "$install_global_bashrc" == yes ]]; then
-        if [[ -f "/etc/bash.bashrc" ]]; then
-                sudo cp "$(pwd)/.bash/etc/rc" "/etc/bash.bashrc"
-        fi
-
-        if [[ -f "/etc/bashrc" ]]; then
-                sudo cp "$(pwd)/.bash/etc/rc" "/etc/bashrc"
-        fi
-fi
-
-if [[ -f "$HOME/.bash_profile" ]]; then
-        echo "Skipping .bash_profile. It already exists"
-else
-        ln -s "$(pwd)/.bash/profile" "$HOME"/.bash_profile;
-fi
-
 if [ -f "$HOME/.bashrc" ]; then
         echo "Skipping .bashrc. It already exists"
 else
